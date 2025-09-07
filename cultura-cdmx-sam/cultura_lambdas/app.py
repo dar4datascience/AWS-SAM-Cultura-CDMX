c9cded9b120aad3c3d4fad1c3034c55285d9df60
dar4datascience <danielamieva@dar4datascience.com>
2025-09-06 21:51:22 -0600

    checj point opklaywreitfhgtr

---
+++ cultura-cdmx-sam/cultura_lambdas/app.py
@@ -0,0 +1,24 @@
+from playwright.sync_api import sync_playwright
+
+def handler(event, context):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(
+            headless=True,
+            args=[
+                "--no-sandbox",
+                "--disable-setuid-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-gpu",
+                "--single-process"
+            ]
+        )
+        # Create a new context per invocation
+        context = browser.new_context()
+        page = context.new_page()
+
+        page.goto("https://cartelera.cdmx.gob.mx/busqueda")
+        content = page.content()
+
+        browser.close()
+
+    return {"statusCode": 200, "body": content}
